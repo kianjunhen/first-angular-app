b543c591ea9089f9a650a98daa97fc2a501cecca
kianjun <kjhen0929@gmail.com>
2026-05-06 17:42:23 +0800

    updated: read and write data from localstorage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -36,6 +36,13 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks (userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -48,9 +55,15 @@ export class TasksService {
       title: taskData.title,
       dueDate: taskData.date,
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks( ) {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
